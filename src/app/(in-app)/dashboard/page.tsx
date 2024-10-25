2bd2b7b0818d2b049f8d312f14c8088c0f350e8d
jeroen.derks@labela.nl <jeroenderks88@gmail.com>
2024-10-25 16:48:37 +0200

    feat: add date filters

--- src/app/(in-app)/dashboard/page.tsx
+++ src/app/(in-app)/dashboard/page.tsx
@@ -1,18 +1,14 @@
-import { getParkingSessions } from "lib/parking-sessions";
 import { getParkingSpaces } from "lib/parking-spaces";
 import { ParkingOverview } from "modules/ParkingOverview";
 import { ParkingSessions } from "modules/ParkingSessions";
 
 export default async function DashboardPage() {
-  const [parkingSpaces, parkingSessions] = await Promise.all([
-    getParkingSpaces(),
-    getParkingSessions(),
-  ]);
+  const parkingSpaces = await getParkingSpaces();
 
   return (
     <>
       <ParkingOverview {...{ parkingSpaces }} />
-      <ParkingSessions {...{ parkingSessions }} />
+      <ParkingSessions />
     </>
   );
 }
--- src/modules/ParkingSessions/index.tsx
+++ src/modules/ParkingSessions/index.tsx
@@ -1,16 +1,58 @@
+"use client";
+
+import { useState } from "react";
 import { Card } from "components/Card";
 import { ParkingSessionsTable } from "./ParkingSessionsTable";
 import * as i from "types";
 
-export function ParkingSessions({ parkingSessions }: ParkingSessionsProps) {
-  parkingSessions.forEach(({ vehicleType }) => console.log(vehicleType));
+const defaultFilter = {
+  startDate: "2024-10-01",
+  endDate: "2024-10-01",
+};
+export function ParkingSessions() {
+  const [parkingSessions, setParkingSessions] = useState([]);
+  const [filters, setFilters] = useState(defaultFilter);
+
+  const handleFormChange = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log(e);
+  };
+
+  const handleFilterChange = (
+    key: string,
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setFilters({ ...filters, [key]: e.target.value });
+  };
+
+  console.log(filters);
+
   return (
     <Card className="mt-12" title="Parking sessions">
+      <form onChange={(e) => handleFormChange(e)}>
+        <div>
+          <label htmlFor="startDate" className="w-[100px]">
+            Start:
+          </label>
+          <input
+            type="date"
+            id="startDate"
+            name="startDate"
+            className="text-gray-950"
+            onChange={(e) => handleFilterChange("startDate", e)}
+            value={filters.startDate}
+          />
+        </div>
+        <label htmlFor="endDate">End:</label>
+        <input
+          type="date"
+          id="endDate"
+          name="endDate"
+          className="text-gray-950"
+          onChange={(e) => handleFilterChange("endDate", e)}
+          value={filters.endDate}
+        />
+      </form>
       <ParkingSessionsTable {...{ parkingSessions }} />
     </Card>
   );
 }
-
-type ParkingSessionsProps = {
-  parkingSessions: i.ParkingSession[];
-};
